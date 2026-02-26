ddd6e77dffadddaf0f743c02cca30f05c1242791
SaadiaAjbbar <ajbbarsaa6@gmail.com>
2026-02-26 06:24:28 +0000

    creation de model Paiement

--- src/app/Models/Paiement.php
+++ src/app/Models/Paiement.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Paiement extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'depense_participant_id', 'montant', 'date_paiement'
+    ];
+
+    public function depenseParticipant()
+    {
+        return $this->belongsTo(DepenseParticipant::class);
+    }
 }
