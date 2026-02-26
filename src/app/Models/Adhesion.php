b7e4662ae21acf086cc51ffc190412ba868629b3
SaadiaAjbbar <ajbbarsaa6@gmail.com>
2026-02-26 06:19:06 +0000

    creation de model Adhesion

--- src/app/Models/Adhesion.php
+++ src/app/Models/Adhesion.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Adhesion extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'colocation_id', 'role', 'left_at'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
 }
