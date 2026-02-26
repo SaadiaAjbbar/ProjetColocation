0969bd8750ec6f6b3d9b28b86c7d428c2982354c
SaadiaAjbbar <ajbbarsaa6@gmail.com>
2026-02-26 06:18:19 +0000

    creation de model Colocation

--- src/app/Models/Colocation.php
+++ src/app/Models/Colocation.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Colocation extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 'owner_id', 'status'
+    ];
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+
+    public function adhesions()
+    {
+        return $this->hasMany(Adhesion::class);
+    }
+
+    public function depenses()
+    {
+        return $this->hasMany(Depense::class);
+    }
+
+    public function invitations()
+    {
+        return $this->hasMany(Invitation::class);
+    }
 }
