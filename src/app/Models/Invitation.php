842d57ada65cba3c05240fa3a54c8c43abb7f761
SaadiaAjbbar <ajbbarsaa6@gmail.com>
2026-02-26 06:19:36 +0000

    creation de model Invitation

--- src/app/Models/Invitation.php
+++ src/app/Models/Invitation.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Invitation extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'email', 'token', 'colocation_id', 'invited_by', 'accepted'
+    ];
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function inviter()
+    {
+        return $this->belongsTo(User::class, 'invited_by');
+    }
 }
