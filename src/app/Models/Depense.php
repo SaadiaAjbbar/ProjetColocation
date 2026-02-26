3ff67ad9a179732b2979f9c5c330f273b1f50ef1
SaadiaAjbbar <ajbbarsaa6@gmail.com>
2026-02-26 06:21:35 +0000

    creation de model Depense

--- src/app/Models/Depense.php
+++ src/app/Models/Depense.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Depense extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nom', 'montant', 'date', 'colocation_id', 'payer_id', 'category_id'
+    ];
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function payer()
+    {
+        return $this->belongsTo(User::class, 'payer_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function participants()
+    {
+        return $this->hasMany(DepenseParticipant::class);
+    }
 }
