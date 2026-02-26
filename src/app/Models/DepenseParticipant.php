d256c21b7c1484fce6cdfd2dd9030d71ba94faaf
SaadiaAjbbar <ajbbarsaa6@gmail.com>
2026-02-26 06:22:23 +0000

    creation de model DepenseParticipant

--- src/app/Models/DepenseParticipant.php
+++ src/app/Models/DepenseParticipant.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DepenseParticipant extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'depense_id', 'user_id', 'montant_du'
+    ];
+
+    public function depense()
+    {
+        return $this->belongsTo(Depense::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function paiements()
+    {
+        return $this->hasMany(Paiement::class);
+    }
 }
